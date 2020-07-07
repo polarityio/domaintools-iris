96f70b51305286c62a72469d440e9d327e069a53
Ed <ed@polarity.io>
2020-07-06 23:13:52 -0400

    Fix frozen overlay and don't display registration details if they don't exist

--- components/block.js
+++ components/block.js
@@ -2,7 +2,25 @@
 polarity.export = PolarityComponent.extend({
   details: Ember.computed.alias('block.data.details'),
   result: Ember.computed.alias('details.result'),
-
+  hasRegistrationDetails: Ember.computed(
+    'result.registrant_org.value',
+    'result.registrar.value',
+    'result.registrant_contact',
+    function () {
+      if (
+        this.get('result.registrant_org.value') ||
+        this.get('result.registrar.value') ||
+        this.get('result.registrant_contact.name.value') ||
+        this.get('result.registrant_contact.org.value') ||
+        this.get('result.registrant_contact.org.value') ||
+        this.get('result.registrant_contact.city.value') ||
+        this.get('result.registrant_contact.country.value')
+      ) {
+        return true;
+      }
+      return false;
+    }
+  ),
   redThreat: '#fa5843',
   greenThreat: '#7dd21b',
   yellowThreat: '#ffc15d',
@@ -17,26 +35,21 @@ polarity.export = PolarityComponent.extend({
   elementRadius: 20,
   elementStrokeWidth: 4,
 
-  elementColor: Ember.computed('details', function() {
+  elementColor: Ember.computed('result.domain_risk.risk_score', function () {
     return this._getThreatColor(this.result.domain_risk.risk_score);
   }),
 
-  elementStrokeOffset: Ember.computed('details', 'elementCircumference', function() {
+  elementStrokeOffset: Ember.computed('result.domain_risk.risk_score', 'elementCircumference', function () {
     return this._getStrokeOffset(this.result.domain_risk.risk_score, this.elementCircumference);
   }),
 
-  threatCircumference: Ember.computed('threatRadius', function() {
+  threatCircumference: Ember.computed('threatRadius', function () {
     return 2 * Math.PI * this.get('threatRadius');
   }),
 
-  elementCircumference: Ember.computed('elementCircumference', function() {
+  elementCircumference: Ember.computed('elementRadius', function () {
     return 2 * Math.PI * this.get('elementRadius');
   }),
-  actions: {
-    subtract(a, b) {
-      return a - b;
-    },
-  },
   _getStrokeOffset(ticScore, circumference) {
     let progress = ticScore / 100;
     return circumference * (1 - progress);
